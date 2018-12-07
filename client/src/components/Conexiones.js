3f5610f9367d99f51b76b21d799ca167ed2f13fa
canozo <canwoahzo@gmail.com>
2018-12-07 01:47:43 -0600

    Interfaz

--- client/src/components/Conexiones.js
+++ client/src/components/Conexiones.js
@@ -1,9 +1,133 @@
 import React from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 class Conexiones extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+
+    this.state = {
+      nombreInstanciaOrigen: '',
+      nombreBaseDatosOrigen: '',
+      puertoOrigen: '',
+      nombreUsuarioOrigen:'',
+      passOrigen:'',
+      nombreInstanciaDestino: '',
+      nombreBaseDatosDestino: '',
+      puertoDestino: '',
+      nombreUsuarioDestino:'',
+      passDestino:'',
+    };
+  }
+
   render() {
+    const { nombreInstanciaOrigen, nombreBaseDatosOrigen, puertoOrigen, nombreUsuarioOrigen, passOrigen, nombreInstanciaDestino, nombreBaseDatosDestino, puertoDestino, nombreUsuarioDestino, passDestino } = this.state;
+
     return (
-      <h1>Conexiones</h1>
+      <Form>
+        <div className="row">
+          <div className="col-md-2" />
+          <div className="col-md-3">
+            <h1>Base de Datos Origen</h1>
+            <FormGroup>
+              <Label for="nombreInstanciaOrigen">Nombre Instancia</Label>
+              <Input
+                value={nombreInstanciaOrigen}
+                onChange={event => this.setState({ nombreInstanciaOrigen: event.target.value })}
+                name="nombre-instanciaOrigen"
+                id="nombreInstanciaOrigen"
+                // placeholder="Instancia Origen"
+              />
+              <Label for="nombreBaseDatosOrigen">Nombre Base de Datos</Label>
+              <Input
+                value={nombreBaseDatosOrigen}
+                onChange={event => this.setState({ nombreBaseDatosOrigen: event.target.value })}
+                name="nombre-baseOrigen"
+                id="nombreBaseDatosOrigen"
+                // placeholder="Proyecto 2"
+              />
+              <Label for="puertoOrigen">Puerto</Label>
+              <Input
+                value={puertoOrigen}
+                onChange={event => this.setState({ puertoOrigen: event.target.value })}
+                name="vpuertoOrigen"
+                id="puertoOrigen"
+                // placeholder="0808"
+              />
+              <Label for="nombreUsuarioOrigen">Nombre Usuario</Label>
+              <Input
+                value={nombreUsuarioOrigen}
+                onChange={event => this.setState({ nombreUsuarioOrigen: event.target.value })}
+                name="nombre-usuarioOrigen"
+                id="nombreUsuarioOrigen"
+                //  placeholder="Crysapa"
+              />
+              <Label for="passOrigen">Password</Label>
+              <Input
+                type="password"
+                value={passOrigen}
+                onChange={event => this.setState({ passOrigen: event.target.value })}
+                name="passwordOrigen"
+                id="passOrigen"
+                // placeholder="123"
+              />
+            </FormGroup>
+            <Button>Probar</Button>
+          </div>
+          <div className="col-md-2" />
+          <div className="col-md-3">
+            <h1>Base de Datos Destino</h1>
+              <FormGroup>
+                <Label for="nombreInstanciaDestino">Nombre Instancia</Label>
+                <Input
+                  value={nombreInstanciaDestino}
+                  onChange={event => this.setState({ nombreInstanciaDestino: event.target.value })}
+                  name="nombre-instanciaDestino"
+                  id="nombreInstanciaDestino"
+                  // placeholder="Instancia Destino"
+                />
+                <Label for="nombreBaseDatosDestino">Nombre Base de Datos</Label>
+                <Input
+                  value={nombreBaseDatosDestino}
+                  onChange={event => this.setState({ nombreBaseDatosDestino: event.target.value })}
+                  name="nombre-baseDestino"
+                  id="nombreBaseDatosDestino"
+                  // placeholder="Proyecto 2"
+                />
+                <Label for="puertoDestino">Puerto</Label>
+                <Input
+                  value={puertoDestino}
+                  onChange={event => this.setState({ puertoDestino: event.target.value })}
+                  name="vpuertoDestino"
+                  id="puertoDestino"
+                  // placeholder="0808"
+                />
+                <Label for="nombreUsuarioDestino">Nombre Usuario</Label>
+                <Input
+                  value={nombreUsuarioDestino}
+                  onChange={event => this.setState({ nombreUsuarioDestino: event.target.value })}
+                  name="nombre-usuarioDestino"
+                  id="nombreUsuarioDestino"
+                  //  placeholder="Crysapa"
+                />
+                <Label for="passDestino">Password</Label>
+                <Input
+                  value={passDestino}
+                  onChange={event => this.setState({ passDestino: event.target.value })}
+                  type="password"
+                  name="passwordDestino"
+                  id="passDestino"
+                  // placeholder="123"
+                />
+              </FormGroup>
+            <Button>Probar</Button>
+          </div>
+          <div className="col-md-2" />
+        </div>
+        <FormGroup style={{ display: 'flex', justifyContent: 'center' }}>
+          <Button>Guardar</Button>
+        </FormGroup>
+      </Form>
     );
   }
 }
--- client/src/components/Replicacion.js
+++ client/src/components/Replicacion.js
@@ -1,9 +1,147 @@
 import React from 'react';
+import { Button, CustomInput, Form, FormGroup, Label } from 'reactstrap';
 
 class Replicacion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+
+    this.replicar = this.replicar.bind(this);
+    this.noReplicar = this.noReplicar.bind(this);
+    this.checkearSinReplicar = this.checkearSinReplicar.bind(this);
+    this.checkearReplicando = this.checkearReplicando.bind(this);
+
+    this.state = {
+      noReplicadas: ['tbl_empleados', 'tbl_alumnos', 'tbl_secciones'],
+      replicadas: ['tbl_aulas'],
+      checkedSinReplicar: {},
+      checkedReplicando: {},
+    };
+  }
+
+  checkearSinReplicar(event) {
+    const { checkedSinReplicar } = this.state;
+    checkedSinReplicar[event.target.id] = event.target.checked;
+    this.setState({ checkedSinReplicar });
+  }
+
+  checkearReplicando(event) {
+    const { checkedReplicando } = this.state;
+    checkedReplicando[event.target.id] = event.target.checked;
+    this.setState({ checkedReplicando });
+  }
+
+  replicar() {
+    const { noReplicadas, replicadas, checkedSinReplicar } = this.state;
+
+    let checkedSinReplicarRes = Object.assign({}, checkedSinReplicar);
+    let noReplicadasRes = noReplicadas.slice();
+    let replicadasRes = replicadas.slice();
+
+    for (const key in checkedSinReplicar) {
+      if (checkedSinReplicar[key] === true) {
+        noReplicadasRes = noReplicadasRes.filter(e => e !== key);
+        replicadasRes.push(key);
+        checkedSinReplicarRes[key] = false;
+      }
+    }
+
+    this.setState({
+      noReplicadas: noReplicadasRes,
+      replicadas: replicadasRes,
+      checkedSinReplicar: checkedSinReplicarRes,
+    });
+  }
+
+  noReplicar() {
+    const { noReplicadas, replicadas, checkedReplicando } = this.state;
+
+    let checkedReplicandoRes = Object.assign({}, checkedReplicando);
+    let noReplicadasRes = noReplicadas.slice();
+    let replicadasRes = replicadas.slice();
+
+    for (const key in checkedReplicando) {
+      if (checkedReplicando[key] === true) {
+        replicadasRes = replicadasRes.filter(e => e !== key);
+        noReplicadasRes.push(key);
+        checkedReplicandoRes[key] = false;
+      }
+    }
+
+    this.setState({
+      noReplicadas: noReplicadasRes,
+      replicadas: replicadasRes,
+      checkedReplicando: checkedReplicandoRes,
+    });
+  }
+
   render() {
+    const { replicadas, noReplicadas, checkedReplicando, checkedSinReplicar } = this.state;
+
     return (
-      <h1>Replicacion</h1>
+      <Form>
+        {/* <h1 style={{ display: 'flex', justifyContent: 'center' }}>Replicacion</h1> */}
+        <div className="row">
+          <div className="col-md-1" />
+          <div className="col-md-2">
+            <FormGroup>
+              <Label for="sinReplicar">Sin Replicar</Label>
+              {noReplicadas.map(tabla => (
+                <CustomInput
+                  type="checkbox"
+                  key={tabla}
+                  id={tabla}
+                  label={tabla}
+                  onChange={this.checkearSinReplicar}
+                  value={checkedSinReplicar[tabla]}
+                />
+              ))}
+            </FormGroup>
+          </div>
+          <div className="col-md-2" />
+          <div className="col-md-2">
+            <h1 style={{ color: '#ffffff' }}>Replicando</h1>
+            <FormGroup>
+              <Button onClick={this.replicar}>{'>>'}</Button>
+            </FormGroup>
+            <FormGroup>
+              <Button onClick={this.noReplicar}>{'<<'}</Button>
+            </FormGroup>
+          </div>
+          <div className="col-md-2" />
+          <div className="col-md-2">
+            <FormGroup>
+              <Label for="replicando">Replicando</Label>
+              {replicadas.map(tabla => (
+                <CustomInput
+                  type="checkbox"
+                  key={tabla}
+                  id={tabla}
+                  label={tabla}
+                  onChange={this.checkearReplicando}
+                  value={checkedReplicando[tabla]}
+                />
+              ))}
+            </FormGroup>
+          </div>
+          <div className="col-md-1" />
+        </div>
+        <div className="row">
+          <div className="col-md-4" />
+          <div className="col-md-1">
+            <FormGroup>
+              <Button>Guardar</Button>
+            </FormGroup>
+          </div>
+          <div className="col-md-1" />
+          <div className="col-md-1">
+            <FormGroup>
+              <Button>Cancelar</Button>
+            </FormGroup>
+          </div>
+          <div className="col-md-5" />
+        </div>
+      </Form>
     );
   }
 }
