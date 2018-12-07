0e1ed4772e0dd0029bdbca6cc39e8a9379353ce2
canozo <canwoahzo@gmail.com>
2018-12-07 01:57:10 -0600

    Portada simple

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,14 @@ import React from 'react';
 class Home extends React.Component {
   render() {
     return (
-      <h1>Home</h1>
+      <div style={{ padding: '70px' }}>
+        <h1>Proyecto 2, BDII</h1>
+        <br />
+        <h2>Presentado por:</h2>
+        <h3>Javier Cano</h3>
+        <h3>Nohelia Euceda</h3>
+        <h3>Crysthel Aparicio</h3>
+      </div>
     );
   }
 }
