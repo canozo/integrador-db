e9e23631e66eab8ace64ebe924886b2b757db298
canozo <canwoahzo@gmail.com>
2018-12-07 02:30:37 -0600

    Pruebas conexiones

--- conexiones.js
+++ conexiones.js
@@ -1,10 +1,44 @@
+const mysql = require('mysql');
+const mssql = require('mssql');
+
 const conexiones = {
   mysql: undefined,
   mssql: undefined,
 };
 
-conexiones.connectMysql = (instancia, db, puerto, usuario, password) => {};
+conexiones.connectMysql = (instancia, db, puerto, usuario, password) => {
+  const config = {
+    host: instancia,
+    port: puerto,
+    database: db,
+    user: usuario,
+    password: password
+  };
 
-conexiones.connectMssql = (instancia, db, puerto, usuario, password) => {};
+  const conexion = mysql.createConnection(config);
+
+  conexion.connect(err => {
+    if (err) {
+      console.log('Error conectado a mysql', err);
+      return;
+    }
+
+    console.log('Conexion establecida con mysql');
+    conexiones.mysql = conexion;
+  });
+};
+
+conexiones.connectMssql = (instancia, db, puerto, usuario, password) => {
+  const config = {
+    server: instancia,
+    port: puerto,
+    database: db,
+    user: usuario,
+    password: password,
+  };
+
+  // const conexion = mssql.connect(config);
+  const conexion = mssql.connect(config);
+};
 
 module.exports = conexiones;
